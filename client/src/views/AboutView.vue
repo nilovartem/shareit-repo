b859ef69b59051e4c547c73a44984c21f174ffe4
nilovartem <nilovtema5@gmail.com>
2023-01-09 22:31:49 +0300

    Добавил элементы в стандартный шаблон Vue.js

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Справка, сказано же!</h1>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Add from '../components/Add.vue'
+</script>
+
+<template>
+  <main>
+    <Add hyperlink="https://www.w3schools.com" />
+  </main>
+</template>
