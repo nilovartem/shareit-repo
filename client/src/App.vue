d7b96007a8b51454ec883ae032567b32167494d3
nilovartem <nilovtema5@gmail.com>
2023-01-12 19:03:10 +0300

    Изменил поведение роутера для новой страницы

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,63 +2,9 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Add from './components/Add.vue';
 import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <div>
-    <nav>
-      <RouterLink to="/">Главная</RouterLink>
-      <RouterLink to="/about">Справка</RouterLink>
-    </nav>
-    <RouterView />
-  </div>
-   
+    <RouterView></RouterView>
 </template>
-
-<style scoped>
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
